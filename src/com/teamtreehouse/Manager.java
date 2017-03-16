ebd5d92ea86250725e8cf5a8394e77bda401c5d3
mark <ulatec@gmail.com>
2017-03-16 10:20:06 -0700

    Initial commit

---
+++ src/com/teamtreehouse/Manager.java
@@ -0,0 +1,9 @@
+package com.teamtreehouse;
+/**
+ * Created by mark on 3/16/2017.
+ */
+public class Manager {
+    public Manager(){
+
+    }
+}
---
+++ src/com/teamtreehouse/model/Team.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.model;
+
+import java.util.List;
+
+/**
+ * Created by mark on 3/16/2017.
+ */
+public class Team {
+    private List<Player> mPlayers;
+    public Team(){
+
+    }
+}
